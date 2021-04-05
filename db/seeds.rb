5c4a94e11b14c18ba5db8b9af3b6622c2efe121f
github username <github email address>
2021-04-05 12:10:27 -0700

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ride.create(departure: "San Francisco", destination: "New-York", date: Time.now)
+Ride.create(departure: "San Francisco", destination: "San Diego", date: Time.now)
+Ride.create(departure: "San Diego", destination: "San Francisco", date: Time.now)
+Ride.create(departure: "Novato", destination: "Lake Tahoe", date: Time.now)
+Ride.create(departure: "Santa Rosa", destination: "Los Angeles", date: Time.now)
